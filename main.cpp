2b289059a0125628930fb822047f1239faedd280
Eczbek <eczbek.void@gmail.com>
2023-08-05 01:17:26 -0400

    Add ants

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <xieite/xieite.hpp>
 
 // Here be dragons
+// Now with ants?
 
 struct Entity {
 	double mass;
@@ -29,7 +30,7 @@ int main() {
 	auto distZ = std::uniform_int_distribution<int>(0, 255);
 
 	std::vector<Entity> entities;
-	for (int i = 0; i < 3; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		entities.push_back(Entity(distW(rng), xieite::geometry::Point(distX(rng), distX(rng)), xieite::geometry::Point(distY(rng), distY(rng)), xieite::graphics::Color(distZ(rng), distZ(rng), distZ(rng))));
 	}
 
@@ -42,25 +43,29 @@ int main() {
 				}
 				const double distanceX = (entity2.position.x - entity1.position.x);
 				const double distanceY = (entity2.position.y - entity1.position.y);
-				entity1.velocity.x += entity1.mass * entity2.mass / distanceX * std::abs(distanceX);
-				entity1.velocity.y += entity1.mass * entity2.mass / distanceY * std::abs(distanceY);
+				entity1.velocity.x += entity1.mass * entity2.mass / distanceX /* * std::abs(distanceX) */ / entities.size();
+				entity1.velocity.y += entity1.mass * entity2.mass / distanceY /* * std::abs(distanceY) */ / entities.size();
+				entity1.velocity.x *= 0.99;
+				entity1.velocity.y *= 0.99;
 			}
 		}
 
-		double centerX = 0;
-		double centerY = 0;
+		// double centerX = 0;
+		// double centerY = 0;
 		for (Entity& entity : entities) {
-			centerX += entity.position.x += entity.velocity.x;
-			centerY += entity.position.y += entity.velocity.y;
+			// centerX += entity.position.x += entity.velocity.x;
+			// centerY += entity.position.y += entity.velocity.y;
+			entity.position.x = std::fmod(entity.position.x + entity.velocity.x + screenSize.column, screenSize.column);
+			entity.position.y = std::fmod(entity.position.y + entity.velocity.y + screenSize.row, screenSize.row);
 		}
-		centerX /= entities.size();
-		centerY /= entities.size();
+		// centerX /= entities.size();
+		// centerY /= entities.size();
 
 		xieite::system::terminal.clearScreen();
 
 		for (Entity& entity : entities) {
-			const double positionX = entity.position.x - centerX + screenSize.column / 2;
-			const double positionY = entity.position.y - centerY + screenSize.row / 2;
+			const double positionX = entity.position.x;// - centerX + screenSize.column / 2;
+			const double positionY = entity.position.y;// - centerY + screenSize.row / 2;
 			if ((positionX < 0) || (positionX >= screenSize.column) || (positionY < 0) || (positionY >= screenSize.row)) {
 				continue;
 			}
